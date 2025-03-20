bdc2b38d65def57d168ca4865eb3c5760ed61b54
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-20 12:17:51 +0100

    Update Journey component padding for improved layout consistency

--- components/about/Journey.tsx
+++ components/about/Journey.tsx
@@ -1,7 +1,7 @@
 
 const Journey = () => {
     return (
-        <div className="pb-[50px] pt-[80px] px-[100px] max-900:px-[20px]" >
+        <div className="pb-[50px] pt-[60px] px-[100px] max-900:px-[20px]" >
             <div className=" border-b border-dark12">
                 <p className="text-grey50 text-[18px]">Journey</p>
                 <h1 className="text-[38px] pb-[40px] ">Damien's Journey - A Timeline</h1>
