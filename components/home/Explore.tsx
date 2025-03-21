ca75aa1d4cef615bca27d1af09ddfee11b111af8
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-21 20:01:38 +0100

    Refactor Explore, Commercial, Event, and Portrait components; remove unused navigation elements and clean up layout Add Swiper library to enhance carousel functionality in portfolio sections Update testimonial data; add new reviews and remove unnecessary fields Update Next.js configuration to enable experimental turbo features

--- components/home/Explore.tsx
+++ components/home/Explore.tsx
@@ -14,18 +14,7 @@ const Explore = () => {
                     <h1 className="text-[38px] max-900:mb-[10px] ">Explore My photography work.</h1>
                 </div>
                 <div className="flex items-center gap-5">
-                    <div className="flex gap-2 border border-dark12 px-2 py-1 rounded-[100px] 
-                         max-900:absolute max-900:bottom-10 max-900:left-1/2 max-900:-translate-x-1/2">
-
-                        <div className="bg-[url('/images/circle.png')] w-[45px] h-[45px] bg-cover flex justify-center items-center cursor-pointer">
-                            <BiChevronLeft className="text-white text-3xl" />
-                        </div>
-
-                        <div className="bg-[url('/images/circle.png')] w-[45px] h-[45px] bg-cover flex justify-center items-center cursor-pointer">
-                            <BiChevronRight className="text-white text-3xl" />
-                        </div>
-
-                    </div>
+                   
 
                     <ButtonWithArrow text="View All Services" />
 
--- components/portfolio/Portrait.tsx
+++ components/portfolio/Portrait.tsx
@@ -1,33 +1,17 @@
 import { portraitprojects } from "@/data/projects";
 import ProjectGallery from "@components/layout/ProjectGallery";
-import ButtonWithArrow from "@components/ui/Button";
-import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+
 
 
 
 const Portrait = () => {
     return (
-        <div className="relative max-900:mb-[40px] border-b border-dark12">
-            <div className="flex  items-center justify-between pb-[20px] px-[100px] max-900:px-[20px] max-900:block " >
+        <div className="relative mb-[40px] border-b border-dark12">
+            <div className=" pb-[20px] px-[100px] max-900:px-[20px] max-900:block " >
                 <div>
                     <h1 className="text-[30px] max-900:text-[25px] max-900:mb-[10px] text-grey50">Portraits Photography</h1>
                 </div>
-                <div className="flex items-center gap-5">
-                    <div className="flex gap-2 border border-dark12 px-2 py-1 rounded-[100px] 
-                         max-900:absolute max-900:bottom-10 max-900:left-1/2 max-900:-translate-x-1/2">
-
-                        <div className="bg-[url('/images/circle.png')] w-[45px] h-[45px] bg-cover flex justify-center items-center cursor-pointer">
-                            <BiChevronLeft className="text-white text-3xl" />
-                        </div>
-
-                        <div className="bg-[url('/images/circle.png')] w-[45px] h-[45px] bg-cover flex justify-center items-center cursor-pointer">
-                            <BiChevronRight className="text-white text-3xl" />
-                        </div>
-
-                    </div>
-
-
-                </div>
+               
 
             </div>
 
