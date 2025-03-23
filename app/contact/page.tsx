0042b5ddd18444e807759a2c21246fc35d9d2b05
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-23 21:44:00 +0100

    Add Contact page and HeaderContact component; implement layout and content structure

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import HeaderContact from "@components/contact/HeaderContact";
+
+
+export default function ContactPage() {
+    return (
+        <div className="home bg-dark03 uppercase">
+
+            <HeaderContact />
+        </div>
+    );
+}
